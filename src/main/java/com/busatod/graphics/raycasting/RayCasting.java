2bfed71ac843c10831388fa167f6b4f0a9461162
dbusato <davidedbusato@gmail.com>
2020-06-27 12:15:15 +0200

    Fixing

--- src/main/java/com/busatod/graphics/raycasting/RayCasting.java
+++ src/main/java/com/busatod/graphics/raycasting/RayCasting.java
@@ -1,5 +1,43 @@
 package com.busatod.graphics.raycasting;
 
-public class RayCasting
+import com.busatod.graphics.app.GraphicsApplication;
+import com.busatod.graphics.app.Settings;
+
+public class RayCasting extends GraphicsApplication
 {
+	public RayCasting()
+	{
+		Settings settings = new Settings();
+		settings.title = "Ray Casting";
+		start(settings);
+	}
+	
+	@Override
+	protected void appInit()
+	{
+	
+	}
+	
+	@Override
+	protected void appUpdate(long elapsedTime)
+	{
+	
+	}
+	
+	@Override
+	protected void appFinishOff()
+	{
+	
+	}
+	
+	@Override
+	protected void appPrintFinalStats()
+	{
+	
+	}
+	
+	public static void main(String[] args)
+	{
+		new RayCasting();
+	}
 }
