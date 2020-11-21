6a9dbabd7a71c5b50f042397ded9b7871f872ab6
dbusato <davidedbusato@gmail.com>
2020-11-21 16:33:34 +0100

    Starting...again

---
+++ src/main/java/raycasting/Main.java
@@ -0,0 +1,37 @@
+package raycasting;
+
+import base.graphics.app.GraphicsApplication;
+import base.graphics.app.Settings;
+
+public class Main extends GraphicsApplication {
+
+	public Main() {
+		Settings settings = new Settings();
+		settings.title = "Java Ray Casting";
+		start(settings);
+	}
+
+	@Override
+	protected void appInit() {
+
+	}
+
+	@Override
+	protected void appUpdate(long elapsedTime) {
+
+	}
+
+	@Override
+	protected void appFinishOff() {
+
+	}
+
+	@Override
+	protected void appPrintFinalStats() {
+
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+}
